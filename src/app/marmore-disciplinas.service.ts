765f54aade5c655e72c598ab3949a0b04b2829f5
GabrielMarmore <gabrielpiraja@hotmail.com>
2022-04-08 21:03:37 -0300

    Feature/disciplinasService

--- src/app/marmore-disciplinas.service.ts
+++ src/app/marmore-disciplinas.service.ts
@@ -1,8 +1,73 @@
 import { Injectable } from '@angular/core';
 
+interface Materias {
+  codigo: string;
+  nome: string;
+  dia: string;
+  hora: string;
+  ementa: string;
+}
 @Injectable()
 export class MarmoreDisciplinasService {
+  list: Array<Materias> = [
+    {
+      codigo: 'ISW012A',
+      nome: 'Desenvolvimento para Dispositivos Móveis I',
+      dia: 'Segunda',
+      hora: '19:00',
+      ementa: '',
+    },
+    {
+      codigo: 'ING011A',
+      nome: 'Inglês V',
+      dia: 'Terça',
+      hora: '19:00',
+      ementa: '',
+    },
+    {
+      codigo: 'ING012A',
+      nome: 'Inglês VI',
+      dia: 'Terça',
+      hora: '20:50',
+      ementa: '',
+    },
+    {
+      codigo: 'AGM004A',
+      nome: 'Negócios e Marketing e Eletrônicos',
+      dia: 'Quarta',
+      hora: '19:00',
+      ementa: '',
+    },
+    {
+      codigo: 'IDS002A',
+      nome: 'Desenvolvimento para Servidores II',
+      dia: 'Quinta',
+      hora: '19:00',
+      ementa: '',
+    },
+    {
+      codigo: 'ITE004A',
+      nome: 'Tópicos Especiais em Sistemas para Internet II',
+      dia: 'Sexta',
+      hora: '19:00',
+      ementa: '',
+    },
+    {
+      codigo: 'ISW011A',
+      nome: 'Projeto de Prototipagem e Testes de Usabilidade',
+      dia: 'Sabado',
+      hora: '08:00',
+      ementa: '',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
+  getList() {
+    return this.list;
+  }
+
+  add() {}
+
+  remove() {}
 }
--- src/app/marmore-lista/marmore-lista.component.ts
+++ src/app/marmore-lista/marmore-lista.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MarmoreDisciplinasService } from '../marmore-disciplinas.service';
 
 @Component({
   selector: 'app-marmore-lista',
   templateUrl: './marmore-lista.component.html',
-  styleUrls: ['./marmore-lista.component.css']
+  styleUrls: ['./marmore-lista.component.css'],
 })
 export class MarmoreListaComponent implements OnInit {
+  constructor(public materias: MarmoreDisciplinasService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
