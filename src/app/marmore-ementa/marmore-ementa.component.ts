9138d8222b9425810def09b3cc25633d16bd011e
GabrielMarmore <gabrielpiraja@hotmail.com>
2022-04-29 20:54:24 -0300

    Feature/route

--- src/app/marmore-ementa/marmore-ementa.component.ts
+++ src/app/marmore-ementa/marmore-ementa.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListEmenta } from '../../list';
 
 @Component({
   selector: 'app-marmore-ementa',
   templateUrl: './marmore-ementa.component.html',
-  styleUrls: ['./marmore-ementa.component.css']
+  styleUrls: ['./marmore-ementa.component.css'],
 })
 export class MarmoreEmentaComponent implements OnInit {
+  materia;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.materia = ListEmenta[params.get('index')];
+    });
   }
-
 }
