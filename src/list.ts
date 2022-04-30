e6da0c9d1d5bd62df100dbde2a26c3670a68d0dc
GabrielMarmore <gabrielpiraja@hotmail.com>
2022-04-29 21:01:46 -0300

    Feature/emata-list

--- src/list.ts
+++ src/list.ts
@@ -1,32 +1,42 @@
 export const ListEmenta = [
   {
     id: 1,
+    nome: 'Desenvolvimento para Dispositivos Móveis I',
     ementa:
       'Introdução e conceitos. Recursos disponíveis para dispositivos móveis na linguagem de marcação de textos. Uso de algum ambiente de desenvolvimento: configuração do ambiente, emulação de dispositivos móveis nos computadores pessoais, interface gráfica, serviços disponíveis, banco de dados nos dispositivos. Desenvolvimento de aplicações que utilizam dados de geolocalização. Integração entre sistemas remotos',
   },
   {
     id: 2,
+    nome: 'Inglês V',
     ementa:
       'Aprofundamento da compreensão e produção oral e escrita por meio da integração das habilidades linguístico-comunicativas. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos socioculturais da língua inglesa.',
   },
   {
     id: 3,
+    nome: 'Inglês VI',
     ementa:
       'Aprimoramento da compreensão e produção oral e escrita por meio da integração das habilidades linguístico-comunicativas. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos socioculturais da língua inglesa.',
   },
   {
     id: 4,
+    nome: 'Negócios e Marketing e Eletrônicos',
     ementa:
       'Conceitos de marketing e de marketing digital. O comportamento do consumidor on-line. Marketing de conteúdo. Marketing nas mídias sociais. E-mail marketing. Marketing viral. Publicidade on-line. Pesquisa de marketing on-line. Marketing e os negócios na Internet. Estudos de caso',
   },
   {
     id: 5,
+    nome: 'Desenvolvimento para Servidores II',
     ementa:
       'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros',
   },
-  { id: 6, ementa: 'Recursos e aplicações da tecnologia escolhida' },
   {
     id: 6,
+    nome: 'Tópicos Especiais em Sistemas para Internet II',
+    ementa: 'Recursos e aplicações da tecnologia escolhida',
+  },
+  {
+    id: 7,
+    nome: 'Projeto de Prototipagem e Testes de Usabilidade',
     ementa:
       'Conceitos, métodos de prototipagem e ferramentas de teste de protótipos. Conceitos de usabilidade e estratégias de design. Métodos de teste de usabilidade. Planejamento de teste de usabilidade. Recrutamento. Projeto de usabilidade. Plano de teste. Sessão de teste de usabilidade. Análise de dados. Comunicação dos resultados. Variação de técnicas',
   },
